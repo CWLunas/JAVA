214d4a0c8283174c4752024c40e8b57b66bc7947
Colin Lunas <120340798+CWLunas@users.noreply.github.com>
2023-07-13 07:37:27 -0800

    Add files via upload

---
+++ Calculator/src/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+    public static void main(String[] args) {
+    	Calculator calculator = new Calculator();
+        calculator.setOperandOne(10.5);
+        calculator.setOperation("+");
+        calculator.setOperandTwo(5.2);
+        calculator.performOperation();
+        double result = calculator.getResults();
+        System.out.println("Result: " + result);
+    }
+}
