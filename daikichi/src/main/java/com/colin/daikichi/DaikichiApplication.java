5a6dfa9a1557a80289fb970988e7d4a4a0fc03e7
Colin Lunas <120340798+CWLunas@users.noreply.github.com>
2023-07-20 18:31:59 -0800

    Add files via upload

---
+++ daikichi/src/main/java/com/colin/daikichi/DaikichiApplication.java
@@ -0,0 +1,13 @@
+package com.colin.daikichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiApplication.class, args);
+	}
+
+}
